813392069397cfadf954cdb7c71b789485effa45
OneOut <redianying@qq.com>
2014-12-06 09:54:01 +0800

    add readme.md

---
+++ 20141205-UseFileReadAndWrite/20141205-UseFileReadAndWrite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20141205-UseFileReadAndWrite
+//
+//  Created by 杜承玖 on 14/12/6.
+//  Copyright (c) 2014年 redianying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20141205-UseFileReadAndWrite/20141205-UseFileReadAndWrite/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20141205-UseFileReadAndWrite
+//
+//  Created by 杜承玖 on 14/12/6.
+//  Copyright (c) 2014年 redianying. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
